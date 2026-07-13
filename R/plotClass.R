482b3b7aac5b40a02c9713746fde8a816d6ca47d
sandrasigismondi <sandra.sigismondi22@gmail.com>
2026-07-13 15:56:19 +0200

    Update plotClass.R

--- R/plotClass.R
+++ R/plotClass.R
@@ -1,1 +1,14 @@
+# Plot unsupervised classification
 
+plotClass <- function(classification,
+                      colors = viridis(4,
+                                       option = "E"),
+                      title = "Classification"){
+  
+  plot(
+    classification$map,
+    col = colors,
+    main = title
+  )
+  
+}
