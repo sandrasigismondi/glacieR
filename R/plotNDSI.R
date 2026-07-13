593299ae61dd3b986bff1ee2b9ba46bb3df3e9c0
sandrasigismondi <sandra.sigismondi22@gmail.com>
2026-07-13 15:54:44 +0200

    Update plotNDSI.R

--- R/plotNDSI.R
+++ R/plotNDSI.R
@@ -1,1 +1,13 @@
+# Plot NDSI
 
+plotNDSI <- function(ndsi,
+                     palette = mako(200),
+                     title = "NDSI"){
+  
+  plot(
+    ndsi,
+    col = palette,
+    main = title
+  )
+  
+}
