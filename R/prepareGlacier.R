0dfa55b89de7607bff65bea77f466d18b5e0274f
sandrasigismondi <sandra.sigismondi22@gmail.com>
2026-07-13 17:41:35 +0200

    Update prepareGlacier.R

--- R/prepareGlacier.R
+++ R/prepareGlacier.R
@@ -1,13 +1,14 @@
 # Prepare images
 
 prepareGlacier <- function(img, boundary){
-  
-  boundary <- terra::vect(boundary)
-  
+
+  if(!inherits(boundary, "SpatVector")){
+    boundary <- terra::vect(boundary)
+  }
+
   img_crop <- crop(img, boundary)
-  
   img_mask <- mask(img_crop, boundary)
-  
+
   return(img_mask)
-  
+
 }
