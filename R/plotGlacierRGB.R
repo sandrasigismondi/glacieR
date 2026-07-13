6e3624c2bb1e2ef9b75dd168251ec47122095054
sandrasigismondi <sandra.sigismondi22@gmail.com>
2026-07-13 15:53:53 +0200

    Update plotGlacierRGB.R

--- R/plotGlacierRGB.R
+++ R/plotGlacierRGB.R
@@ -1,1 +1,14 @@
+# RGB visualization
 
+plotGlacierRGB <- function(img,
+                            r,
+                            g,
+                            b){
+  
+  plotRGB(img,
+          r=r,
+          g=g,
+          b=b,
+          stretch="lin")
+  
+}
