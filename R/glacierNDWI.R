660e8ac1dac6f10710c1a565f572077f8654bd91
sandrasigismondi <sandra.sigismondi22@gmail.com>
2026-07-13 15:55:12 +0200

    Update glacierNDWI.R

--- R/glacierNDWI.R
+++ R/glacierNDWI.R
@@ -1,1 +1,14 @@
+# NDWI
 
+glacierNDWI <- function(img,
+                        green,
+                        nir){
+  
+  ndwi <- (img[[green]]-
+             img[[nir]])/
+    (img[[green]]+
+       img[[nir]])
+  
+  return(ndwi)
+  
+}
