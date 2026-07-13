2e50622a5cfc4b3491382ad8f7d8fc83a0b9c839
sandrasigismondi <sandra.sigismondi22@gmail.com>
2026-07-13 15:55:38 +0200

    Update plotNDWI.R

--- R/plotNDWI.R
+++ R/plotNDWI.R
@@ -1,1 +1,13 @@
+# Plot NDWI
 
+plotNDWI <- function(ndwi,
+                     palette = viridis(200),
+                     title = "NDWI"){
+  
+  plot(
+    ndwi,
+    col = palette,
+    main = title
+  )
+  
+}
