a1320cf22de59250b07efe4bcc3623f02efcee2b
sandrasigismondi <sandra.sigismondi22@gmail.com>
2026-07-13 15:54:17 +0200

    Update glacierNDSI.R

--- R/glacierNDSI.R
+++ R/glacierNDSI.R
@@ -1,1 +1,14 @@
+# NDSI
 
+glacierNDSI <- function(img,
+                        green,
+                        swir){
+  
+  ndsi <- (img[[green]] -
+             img[[swir]]) /
+    (img[[green]] +
+       img[[swir]])
+  
+  return(ndsi)
+  
+}
